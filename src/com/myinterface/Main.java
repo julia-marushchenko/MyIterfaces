4a93ceeef015d0a7b47c9016928657e390baea37
Julia Marushchenko <marushchenko.yulia@gmail.com>
2026-04-05 07:53:46 +0200

    Add files via upload
    Committing Main, StudentImp classesl, and Student interface.

---
+++ src/com/myinterface/Main.java
@@ -0,0 +1,55 @@
+/**
+ *  Java program to demonstrate usage of interfaces.
+ */
+
+package com.myinterface;
+
+/**
+ *  Main class with JVM entry point.
+ */
+public class Main {
+
+    // JVM entry point.
+    public static void main(String[] args) {
+
+        // Creating Student instance.
+        StudentImpl student = new StudentImpl();
+
+        // Marks of student.
+        double[] marks = new double[] {99, 78.5, 88, 95,6, 97};
+
+        // Calling method and printing the result to console.
+        StudentImpl st = new StudentImpl();
+       System.out.println("Average mark is: " + st.getMarks(marks));
+
+    }
+}
+
+// Student interface.
+interface Student {
+
+    // Method to return average value of marks.
+    public double getMarks(double[] marks);
+
+}
+
+class StudentImpl implements Student {
+
+
+    public double getMarks(double[] marks) {
+
+        // Initializing sum of ,arks.
+        double sum = 0;
+
+        // Finding average mark.
+        for (double elements : marks) {
+            sum += elements;
+        }
+
+        // Average mark.
+        double average = sum / marks.length;
+
+        return average;
+    }
+
+}
